b132767785448511efb68c486ae74315927938da
windingwind <33902321+windingwind@users.noreply.github.com>
2024-12-30 21:18:53 +0100

    chore: do not enable debug bridge by default

--- src/utils/ztoolkit.ts
+++ src/utils/ztoolkit.ts
@@ -20,8 +20,10 @@ function initZToolkit(_ztoolkit: ReturnType<typeof createZToolkit>) {
   _ztoolkit.basicOptions.log.disableConsole = env === "production";
   _ztoolkit.UI.basicOptions.ui.enableElementJSONLog = __env__ === "development";
   _ztoolkit.UI.basicOptions.ui.enableElementDOMLog = __env__ === "development";
-  _ztoolkit.basicOptions.debug.disableDebugBridgePassword =
-    __env__ === "development";
+  // Getting basicOptions.debug will load global modules like the debug bridge.
+  // since we want to deprecate it, should avoid using it unless necessary.
+  // _ztoolkit.basicOptions.debug.disableDebugBridgePassword =
+  //   __env__ === "development";
   _ztoolkit.basicOptions.api.pluginID = config.addonID;
   _ztoolkit.ProgressWindow.setIconURI(
     "default",
